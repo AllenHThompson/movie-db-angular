d3c7b4ad173c04a500302fcbab7869ee202b27a4
Allen H. Thompson <allenhthompson1@gmail.com>
2016-10-17 10:54:54 -0400

    Added 'More' button to search through results.

--- movie-app.js
+++ movie-app.js
@@ -1,7 +1,8 @@
+var app = angular.module('my-app',['ngRoute']);
+
 var API_KEY = 'fec8b5ab27b292a68294261bb21b04a5';
 
-var app = angular.module('my-app',['ngRoute']);
-app.config(function($routeProvider){
+app.config(function($routeProvider) {
      $routeProvider
      .when('/', {
           controller: 'MainController',
@@ -18,16 +19,23 @@ app.controller('MainController', function($scope, $http) {
 
      // $scope.imgURL = 'http://image.tmdb.org/t/p/w382';
      // $scope.dataResults = [];
-     $http.get("http://api.themoviedb.org/3/movie/now_playing?api_key=" + API_KEY).success(function(movies){
+     $http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=' + API_KEY).success(function(movies) {
           console.log(movies);
+          console.log(movies.total_pages);
           $scope.movies = movies.results;
           // $scope.movies = movies;
-
      });
+
+     $scope.click = function() {
+          console.log("you clicked");
+     };
+
 });
 
-app.controller('DetailsController', function($scope, $http, $routeParams){
-     $scope.movieID = $routeParams.movieID
+
+
+app.controller('DetailsController', function($scope, $http, $routeParams) {
+     $scope.movieID = $routeParams.movieID;
      var url = 'http://api.themoviedb.org/3/movie/' +
           $routeParams.movieID + '?api_key=' + API_KEY;
      $http.get(url)
